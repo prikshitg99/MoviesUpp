c7872a614342dfb780a4c7317e487d72bb7b7e36
prikshitg99 <prikshitg99@gmail.com>
2025-01-08 21:53:57 +0530

    Version 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+//components
+import Main from './components/Main'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="app-title">MoviesUpp <br/><br/>By Prikshit Grover<br/><br/></h1>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
